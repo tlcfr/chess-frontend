94736a23facd3e5cec84977c142ba9ac00b8b831
tlcfr <tatarinov.lcfr@gmail.com>
2024-10-15 00:03:58 +0300

    Display selected square

--- src/Chessboard.tsx
+++ src/Chessboard.tsx
@@ -1,12 +1,21 @@
 import "./Chessboard.css";
 import board from "/board.svg";
-import React from "react";
+import React, { useState } from "react";
 import PieceDto from "./dto/PieceDto.ts";
+import classNames from "classnames";
 
 function Chessboard({pieces}: { pieces: PieceDto[] }) {
 
+  const [selected, setSelected] = useState<number>();
+
   function handleSquareClick(index: number) {
-    console.log("Clicked square with index:", index);
+    if (index === selected) {
+      setSelected(undefined);
+    } else if (pieces[index]) {
+      setSelected(index);
+    } else {
+      setSelected(undefined);
+    }
   }
 
   function buildSquares(): React.JSX.Element[] {
@@ -15,8 +24,11 @@ function Chessboard({pieces}: { pieces: PieceDto[] }) {
       const rank = [] as React.JSX.Element[];
       for (let fileIndex = 0; fileIndex < 8; fileIndex++) {
         const index = rankIndex * 8 + fileIndex;
+        const squareStyles = classNames("square", {
+          selected: index === selected,
+        });
         rank.push(
-          <div className="square" key={index} onClick={() => handleSquareClick(index)}>
+          <div className={squareStyles} key={index} onClick={() => handleSquareClick(index)}>
             <img src={`/piece/${pieces[index]?.shortName}.svg`} alt="" />
           </div>,
         );
